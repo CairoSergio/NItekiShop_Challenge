7aae1d6f6c0901c0df092e7406699eeb19a4abc8
Cairo Sergio <cairosergiondava@gmail.com>
2023-07-05 12:29:43 -0700

    int: installing all dependences necessary

---
+++ src/Routes/index.jsx
@@ -0,0 +1,24 @@
+import { createBrowserRouter } from "react-router-dom";
+import PageNotFound from "../Screens/404";
+import Cart from "../Screens/Cart";
+import Details from "../Screens/Details";
+import Home from "../Screens/Home";
+
+export const Routes = createBrowserRouter([
+    {
+        path:'/',
+        element:<Home/>
+    },
+    {
+        path:'/Details',
+        element: <Details/>
+    },
+    {
+        path: '/Cart',
+        element: <Cart/>
+    },
+    {
+        path:'*',
+        element: <PageNotFound/>
+    }
+])
---
+++ src/Screens/404/index.jsx
@@ -0,0 +1,5 @@
+export default function PageNotFound(){
+    return(
+        <div>Error 404 | Pagina nao enconrada</div>
+    )
+}
