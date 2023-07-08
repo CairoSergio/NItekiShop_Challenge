6e05c4de32c612836f775815660fd955344c46f7
Cairo Sergio <cairosergiondava@gmail.com>
2023-07-07 22:14:52 -0700

    Some updates

--- src/Screens/404/index.jsx
+++ src/Screens/404/index.jsx
@@ -1,5 +1,14 @@
+import { Button } from "@mui/material";
+
 export default function PageNotFound(){
     return(
-        <div>Error 404 | Pagina nao enconrada</div>
+        <div style={{height:'100vh', flexDirection:'column',gap:'10px',width:'100vw', display:'flex', justifyContent:"center", alignItems:'center'}}>
+            <h3 color="#3F4046">
+                Error 404 | Pagina nao enconrada
+            </h3>
+            <Button href="/" variant="contained">
+                Voltar Para a pagina inicial
+            </Button>
+        </div>
     )
 }
